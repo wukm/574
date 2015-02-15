1e92dff51fdd4ab531d35eddbcebe31aca763783
wukm <lxvkee@gmail.com>
2015-02-14 21:00:52 -0800

    wrote LSclassify according to tutorial

--- hw2/LSclassify.py
+++ hw2/LSclassify.py
@@ -5,21 +5,25 @@ LSClassify.py
 Least squares clasification.
 """
 
+import numpy
+#import scipy.sparse.linalg as sla
+import scipy.linalg
 
-def ls_classify(Xtrain, y, lamb):
+def ls_classify(Xtrain, y, λ):
     """
-    alpha, beta = ls_classify(Xtrain, y, lamb)
+
+    alpha, beta = ls_classify(Xtrain, y, λ)
 
     INPUT
 
     Xtrain -> training data 
     y   -> class labels for training data
-    lamb -> lambda value to use
+    λ -> lambda value to use (`lambda` is a python builtin, haha)
 
     OUTPUT
 
-    alpha   -> scalar
-    beta    -> vector
+    α -> scalar (offset of the separating hyperplane)
+    β -> vector ('slope' of the separating hyperplane)
     
     a python implementation of LSclassify.m
     least squares classification given the training set Xtrain
@@ -33,7 +37,28 @@ def ls_classify(Xtrain, y, lamb):
 
     real comments here later.
     """
-    #return alpha, beta
+    # return α, β
+    
+    x_bar = Xtrain.mean(axis=0) # average for pixels; x_bar.size -> 3
+    y_bar = y.mean() # average class label 
+    
+    #subtract x_bar from each row in Xtrain. broadcasting!
+    X_tilde = Xtrain - x_bar
+    # ditto
+    y_tilde = y - y_bar
+    
+    # woo, I don't have to use sparse!
+    # full_matrices=False is equivalent of the matlab 'econ'
+    U, S, V = scipy.linalg.svd(X_tilde, full_matrices=False)
+
+    # β = V(Σ² + λI)⁻¹Σ(U.T)(y_tilde)
+    c_values = S / (S*S + λ)
+
+    # can't wait for the @ operator in python 3.5
+    β = numpy.dot(V.T, (c_values * numpy.dot(U.T, y_tilde)))
+    α = y_bar - numpy.dot(x_bar, β)
+
+    return α, β
 
 if __name__ == "__main__":
     pass
