7b75ed2d2a4ae6ac4d810480101f5eee6e7f9dac
wukm <lxvkee@gmail.com>
2015-03-12 23:42:42 -0700

    added non-kernel portion

--- hw4/c4_2.py
+++ hw4/c4_2.py
@@ -10,11 +10,37 @@ from scipy.io import loadmat
 import os.path
 from PROJECT import project 
 from kernelSVM import kernel_classify
+from loadMNIST import load as load_mnist
+from SVMclassify import svm_classify
 import numpy
+
 
 # when refactoring, just put this in a function that returns X,y 
 DATADIR = './data/'
 matfile = os.path.join(DATADIR, 'MNIST_SMALL.mat')
+
+def _make_row_norm_matrix(X):
+    """
+    if X is an nxd matrix, return an nxn matrix that
+    looks like
+
+    [[ ||x1||^2 ||x1||^2 ... ||x1||^2 ]
+     [ ||x2||^2 ||x2||^2 ... ||x2||^2 ]
+     ...
+     [ ||xn||^2 ||xn||^2 ... ||xn||^2 ]]
+
+    where xi represents a row of X.
+
+    this subfunction is useful for making the gaussian kernel and
+    testing whats-it matrix N
+    """
+
+    temp = [row.T.dot(row) for row in X]
+
+    Q = numpy.array(temp, ndmin=2).T
+    Q = numpy.tile(Q, (1, X.shape[0]))
+
+    return Q
 
 def _make_gaussian_kernel(X, sigma):
     """
@@ -26,24 +52,35 @@ def _make_gaussian_kernel(X, sigma):
     ... using linear algebra tricks, not sure if this is really better
     """
     
-    temp = [row.T.dot(row) for row in X]
+    Q = _make_row_norm_matrix(X)
 
-    # this is how you make a column vector from a list???
-    Q = numpy.array(temp, ndmin=2).T
-    Q = numpy.tile(Q, (1, img.shape[0]))
     xxt = X.dot(X.T)
 
     M = Q - xxt
 
     # now M contains ||x_i - x_j||^2
-
     M = M + M.T
+
     M = -M / (2*sigma**2)
 
     return numpy.exp(M)
 
+def _make_gaussian_N(Z, X, sigma):
+    """
+    M(x,y) := exp{ - ||x-y||^2 / (2σ^2) }
 
-     
+    returns the matrix N with N_(i,j) = M(z_i, x_j) 
+
+    where z_i are the testing points to be classified
+
+    """
+    QX = _make_row_norm_matrix(X) 
+    QZ = _make_row_norm_matrix(Z)
+    
+    M = QZ + QX.T - 2*Z.T.dot(X)
+    M = -M / (2 * sigma**2 )
+    
+    return numpy.exp(M)
 
 d = loadmat(matfile)
 img, lab = d['img'], d['lab']
@@ -63,6 +100,22 @@ gamma = kernel_classify(M, y, C)
 supp = (0 < abs(gamma)) & (abs(gamma) < C)
 
 s = numpy.nonzero(supp)[0]
+v = M.dot(gamma)
+alpha = (y[s] - v[s]).sum() / s.size
 
-alpha = (y[s] - X[s].dot(beta)).sum() / s.size
+img_t, lab_t = load_mnist([4,7,9], 'test')
+binary = numpy.array(lab_t, ndmin=2).T
+
+#N = _make_gaussian_N(img_t, img, sigma=5)
+
+# make N ... i'm resorting to list comp because the dimensions are bad ...
+sigma = 5
+N =  [[ (zi - xj).T.dot(zi - xj) / (2 * sigma**2) for xj in img] for zi in binary]
+N = numpy.array(N)
+
+binary_est = alpha + N.dot(gamma) > 0
+
+
+# do it without classification trick
+mma = svm_classify(img, y, 7.5)
 
