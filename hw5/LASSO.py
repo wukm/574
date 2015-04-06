4d22fe1af174112f048a72f89e3dd942e249b69b
wukm <lxvkee@gmail.com>
2015-04-06 14:48:13 -0700

    better docstrings for LASSO

--- hw5/LASSO.py
+++ hw5/LASSO.py
@@ -6,11 +6,15 @@ LASSO.py
 a reworking of SVMClassify.py to do compressed sensing
 
 i.e. to solve the minimization
+min { E(x) | x in R^n } with E(x) = λf(x) + g(x)
+        f(x) = ||x||_1
+        g(x) = 1/2 (||Ax-b||_2)^2
 
-min { E(x) | x in R^n }  with E(x) = λ||x||_1 + 1/2 (||Ax-b||_2)^2
+this is also known as the 'lasso problem' or 'basis pursuit'
 
-also known as the 'lasso problem'
+There is only one real public function here. import it (for now) by
 
+from LASSO import lasso
 """
 
 import numpy
@@ -24,8 +28,8 @@ def prox(z, λ, dt):
     z:  a vector (row or column)
     
     output:
-    a vector m (m.shape == z.shape), the solution to solution to
-        min { dt*λ||x||_1 + 1/2 (||x-z||_2)^2 | x in R^n }
+    a vector m (m.shape == z.shape), 
+        m = arg min { dt*λ||x||_1 + 1/2 (||x-z||_2)^2 | x in R^n }
 
     the analytical solution is given by component-wise by
     
@@ -37,24 +41,30 @@ def prox(z, λ, dt):
 
     c = λ * dt
     
-    m =  (z - c)*((z - c) > 0) + (z + c)*((z + c) < 0)
+    # this is equivalent to the analytical sol'n above (check it)
+    m = (z-c > 0)*(z-c) + (z+c < 0)*(z+c)
+
     return m
 
 def lasso_energy(x, A, b, λ):
     """
-    outputs the scalar E(x), with 
+    INPUT 
 
-    E(x) = λ||x||_1 + 1/2 (||Ax-b||_2)^2
+    the arrays x, A, b
+    x in R^(n×1) (e.g. is usually the iterated approximation)
+    A in R^(m×n)
+    b in R^(m×1)
 
-    x in R^(n,1)
-    A in R^(m,n)
-    b in R^(m,1)
+    OUTPUT
 
-    output in R
+    the scalar E(x) = λ||x||_1 + 1/2 (||Ax-b||_2)^2
     """
-    #return λ*norm(x, ord=1) + .5 * (norm(A.dot(x) - b, ord=2)**2)
-    return λ * sum(abs(x)) + .5 * (A.dot(x) - b).T.dot(A.dot(x) - b)
-    
+    # these are equivalent, use whichever
+    energy = λ*norm(x, ord=1) + .5 * (norm(A.dot(x) - b, ord=2)**2)
+    #energy = λ * sum(abs(x)) + .5 * (A.dot(x) - b).T.dot(A.dot(x) - b)
+   
+    return energy
+
 def lasso_gradient(x, A, b):
     """
     g(x) = 1/2 ||Ax -b||^2
@@ -69,8 +79,9 @@ def lasso_gradient(x, A, b):
 
     output in R^(n,1)
     """
-
-    return A.T.dot(A.dot(x) - b)
+    p = A.dot(x) - b
+    p = A.T.dot(p)
+    return p
 
 def line_search(dt, p, x, A, b, λ):
     """
@@ -78,31 +89,36 @@ def line_search(dt, p, x, A, b, λ):
 
     please consider a max iteration, just for deterministic implementation
 
-    i don't know what's going on here, i'm taking the iterated contiion
-    wholesale.
+    i don't know what's going on here tbh. this is a variation of the general
+    backtracking line search algorithm (with Armijo-Goldstein condition)
+
+    en.wikipedia.org/wiki/Backtracking_line_search
     """
-    # see class notes
-    dt = 2.0 * dt
+    # rather than starting in the same place, this will actually increase the
+    # last used step size in the new direction (-p)
+    # while we can get away with it
+    dt *= 2.0
     
-    # initial energy and initial x do not change
+    # initial energy and initial x and initial p do not change
     E = lasso_energy(x, A, b, λ)
+    
+    c = .001 # this is an arbitrary parameter in (0,1)
 
     while True:
         x_new = prox(x - dt*p, λ, dt)
 
         E_new = lasso_energy(x_new, A, b, λ)
 
-        if not (E < E_new + .001*norm(x-x_new)**2):
+        if not (E < E_new + c*norm(x-x_new)**2):
             return dt
         else:
-            dt = dt/2.  # take a smaller timestep and try again
+            dt /= 2.  # take a smaller timestep and try again
 
 
 def lasso(A, b, λ, dt=.001, tol=.000001):
     """
-    solves the minimization
-
-    min { E(x) | x in R^n }  with E(x) = λ||x||_1 + 1/2 (||Ax-b||_2)^2
+    returns x = arg min { E(x) | x in R^n }
+        with E(x) = λ||x||_1 + 1/2 (||Ax-b||_2)^2
 
     also known as the 'lasso problem' using gradient descent with the initial
     approximation 0 in R^(nx1).
@@ -128,35 +144,34 @@ def lasso(A, b, λ, dt=.001, tol=.000001):
     # problem regardless, i believe.
     # b = numpy.array(b).reshape(-1,1)
 
-    # does this help at all?
-    A = A.astype('float64')
-    b = b.astype('float64')
-
     # initial guess is zero in R^n
     x = numpy.zeros((A.shape[1], 1), dtype='float64')
     
 
-    p = lasso_gradient(x, A, b)
-    dt = line_search(dt, p, x, A, b, λ)
 
+    # if you want a MAX_ITERATIONS, use
+    # for i in range(MAX_ITERATIONS):
     for i in count():
         
-        x_new = prox((x - dt*p), λ, dt)
+        p = lasso_gradient(x, A, b)
+        dt = line_search(dt, p, x, A, b, λ)
 
-        # normally it is divided by dt, but i do that below
-        q = (x - x_new)
-
-        if i > 0 and not i % 100:
-            #print("i={}\tnorm(q)={}\tdt={}".format(i,norm(q),dt))
+        x_new = prox(x - dt*p, λ, dt)
 
 
-        # compensate for not dividing q by dt earlier
-        if (norm(q) < tol*dt):
+        if not i % 100 and i > 0:
+            print("i={}\tnorm(q)={}\tdt={}".format(i,norm(q),dt))
+            #pass
+
+
+        # to be used in the stopping condition ||q|| < ε
+        # an alternative stopping condition would be || Ax - b || < ε
+        q = (x_new - x) / dt
+
+        if (norm(q) < tol):
             break
         else:
             x = x_new
-            p = lasso_gradient(x, A, b)
-            dt = line_search(dt, p, x, A, b, λ)
 
         #print("iteration {}:".format(i))
         #print("\tdt={}".format(dt))
@@ -168,7 +183,7 @@ def lasso(A, b, λ, dt=.001, tol=.000001):
         #input()
         
         
-    #print("done in {} iterations".format(i))
+    print("done in {} iterations".format(i))
     #print("norm(q)={} < tol={}".format(norm(q), tol))
 
-    return x_new
+    return x_new # return the latest iteration you have
