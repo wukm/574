71c213ceb5bb5f82493ee85c0b2c71ee790765ec
wukm <lxvkee@gmail.com>
2015-04-08 22:38:34 -0700

    fixed issue where resulting fg/bg images were transposed

--- matlab/hw1.m
+++ matlab/hw1.m
@@ -13,19 +13,29 @@ img = cell2mat(img);
 % then this is the rank 1 approximation of the system
 approx = u*s*v';
 
-% third dimension is 
+%{
+okay to be honest i just mashed my data into these functions until i got
+what i wanted. goal is to turn each column of (approx) and (img-approx)
+into a 512x512 matrix that can be viewed as an image. what follows works
+but i doubt it is the most straightforward, i just heard for loops are
+bad so here's the complicated stuff instead, enjoy
+%}
+
+% third dimension is each individual image (but transposed?) 
 bgs = reshape(approx, 512,512,10);
 
 % a 1x1x10 cell
 bgs = num2cell(bgs, [2 1]);
-
+% this 
+bgs = cellfun(@transpose, bgs, 'UniformOutput', false);
 % now in the same shape as images
 bgs = reshape(bgs,10,1);
 
-% this is easier
+% subtracting the rank1 background from each image yields a foreground 
 fgs = img - approx;
 
+% do the same stuff on columns of the foreground matrix
 fgs = reshape(fgs, 512,512,10);
 fgs = num2cell(fgs, [2 1]);
 fgs = reshape(fgs,10,1);
-
+bgs = cellfun(@transpose, fgs, 'UniformOutput', false);
