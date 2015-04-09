76c275896aa5cbb18aabfec2d2bb71c45a89f985
wukm <lxvkee@gmail.com>
2015-04-09 09:31:27 -0700

    done. need for loops and file saving

--- matlab/hw1_2.m
+++ matlab/hw1_2.m
@@ -1,5 +1,6 @@
 % images is a 7844x784 matrix -- each row is a vectorized image of a 1
-[images, labels] = loadMNIST(1,'all');
+
+[images, labels] = loadMNIST(2,'all');
 
 [u,s,v] = svds(images, 2);
 
@@ -12,3 +13,9 @@ figure(2); imagesc(leads{2}); colormap jet;
 % check if these are correct. does it max over... norm?
 [~, imax] = max(u(:,2));
 [~, imin] = min(u(:,2));
+
+maxu = reshape(images(imax,:), [28 28]);
+minu = reshape(images(imin,:), [28 28]);
+
+figure(3); imagesc(maxu); colormap gray;
+figure(4); imagesc(minu); colormap gray;
