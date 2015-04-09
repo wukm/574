6e716efdcb9bdff5852236d125a11866f44cec1e
wukm <lxvkee@gmail.com>
2015-04-09 09:30:22 -0700

    better comments (migrated from hw1_1)

--- matlab/columns_to_image_list.m
+++ matlab/columns_to_image_list.m
@@ -16,14 +16,42 @@ things i could implement:
 
 [m,n] = size(A);
 
+% check that we can actually form matrices with the requested dimensions
+if m ~= d1*d2
+    error('cannot make matrices with those dimensions for the columns provided');
+end;
+
+% okay to be honest i just mashed stuff into these functions until i got
+% what i wanted. goal is to turn each column of (approx) and (img-approx)
+% into a 512x512 matrix that can be viewed as an image. what follows works
+% but i doubt it is the most straightforward, i just heard for loops are
+% bad so here's the complicated stuff instead, enjoy
+
+% % third dimension is each individual image (but transposed?) 
+% bgs = reshape(approx, 512,512,10);
+% 
+% % a 1x1x10 cell
+% bgs = num2cell(bgs, [2 1]);
+% % this 
+% bgs = cellfun(@transpose, bgs, 'UniformOutput', false);
+% % now in the same shape as images
+% bgs = reshape(bgs,10,1);
+
+
+
 %{
 if m != d1*d2
     throw an error
 end
 %}
 
+% so now each A(:,:,n) is an individual image (but transposed)
 A = reshape(A, d1,d2,n);
+% this will create a 1x1xn cell of d1xd2 matrices
 A = num2cell(A, [2 1]);
+% make it a nx1 cell instead
 A = reshape(A,n,1);
 
+% now transpose the matrices within the cell
+% also wow this function is even better than bsxfun /s
 C = cellfun(@transpose, A, 'UniformOutput', false);
