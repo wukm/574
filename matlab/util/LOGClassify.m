5b6e21ddd251f38821158cc5bd3a8a4b5b62aec7
wukm <lxvkee@gmail.com>
2015-04-10 16:16:32 -0700

    finished hw3

--- matlab/util/LOGClassify.m
+++ matlab/util/LOGClassify.m
@@ -1,8 +1,8 @@
 function [alpha,beta] = LOGClassify(X,y,lambda)
-
+% classify a system (X,y) by minimizing a logarithmic energy
 alpha = 0; beta = zeros(size(X,2),1);
 
-flag = 0; dt = .001; iter = 1; TOL = .001; itmax = 20000;
+flag = 0; dt = .00001; iter = 1; TOL = .001; itmax = 20000;
 while(flag == 0),
     
     iter = iter + 1;
@@ -13,8 +13,8 @@ while(flag == 0),
     dt = LINE_SEARCH(dt,p,alpha,beta,X,y,lambda);
     
     % update alpha and beta via (2)
-    %    
-    %
+    alpha = alpha - dt*p(1);
+    beta = beta - dt*p(2:end);
     
    
     if( norm(p) < TOL || iter > itmax ),
@@ -22,17 +22,27 @@ while(flag == 0),
     end;
     
 end;
+E = ENERGY(alpha, beta, X, y, lambda);
+fprintf('LOGClassify terminated after %d iterations with final energy %f\n', iter, E) 
 
 function p = GRADIENT(alpha,beta,X,y,lambda)
 
     d = size(beta,1);
     p = zeros(d+1,1);
     
+    z = alpha+(X*beta);
+    
+    v1 = (exp(z) ./ (1+exp(z))).*(1-y);
+    v2 = (exp(-z) ./ (1+exp(-z))).*y;
+    v = v1 -v2;
+
+  
+    
     % p(1) should be the derivative of E with respect to alpha
-    p(1) =
+    p(1) = sum(v);
     
     % p(2:end) should be the gradient of E with respect to beta
-    p(2:end) =
+    p(2:end) = X'*v + lambda*beta;
     
 function dt = LINE_SEARCH(dt,p,alpha,beta,X,y,lambda)
 
@@ -41,23 +51,27 @@ function dt = LINE_SEARCH(dt,p,alpha,beta,X,y,lambda)
     E = ENERGY(alpha,beta,X,y,lambda);
     
     % update alpha and beta via (2)
-    %    
-    %
+    alpha_new = alpha - dt*p(1);
+    beta_new = beta - dt*p(2:end);
 
     E_new = ENERGY(alpha_new,beta_new,X,y,lambda);
     
     while( E_new > E - .5*dt*(p'*p) ), % then check inequality (3)
         
         % take a smaller timestep and try again
-        %
-        %
-        %                      
-        
+        dt = 0.5*dt;
+        alpha_new = alpha - dt*p(1);
+        beta_new = beta - dt*p(2:end);
         E_new = ENERGY(alpha_new,beta_new,X,y,lambda);
                               
     end;
 
 function E = ENERGY(alpha,beta,X,y,lambda)
-    
+    % E = sum_{i=1}^{n}[ (log(1+exp(alpha + <x_i,beta>))(1-y_i)
+    %                + (log(1+exp(-(alpha + <x_i,beta>)))y_i ]
     % compute the energy via (1)
-    E = 
+    z = alpha + X*beta;
+    E1 = sum( log(1+exp(z)).*(1-y));
+    E2 = sum( log(1+exp(-z)).*(y));
+    
+    E = E1 + E2 + (lambda/2)*beta'*beta;
