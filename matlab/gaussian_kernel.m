5debd66f26141d46abc07bd8975d5364f29b38c2
wukm <lxvkee@gmail.com>
2015-04-23 21:19:35 -0700

    highlight hates multiline matlab comments

--- matlab/gaussian_kernel.m
+++ matlab/gaussian_kernel.m
@@ -1,28 +1,27 @@
 function W = gaussian_kernel(X,sigma)
     % make the gaussian kernel with parameter sigma
-    %{
-    INPUT
-        X:
-            an nxd matrix (typically the 'system')
-        sigma:
-            a parameter
-    OUTPUT
-        W:
-            an nxn gaussian kernel (defined below)
+    %
+    %   INPUT
+    %       X:
+    %           an nxd matrix (typically the 'system')
+    %       sigma:
+    %           a parameter
+    %   OUTPUT
+    %       W:
+    %           an nxn gaussian kernel (defined below)
 
-    This creates the so-called "gaussian kernel," a similarity matrix with
-    components
-        W_ij := exp{ -(||x_i - x_j||_2)^2 / (2*sigma^2) }
+    %   This creates the so-called "gaussian kernel," a similarity matrix with
+    %   components
+    %       W_ij := exp{ -(||x_i - x_j||_2)^2 / (2*sigma^2) }
+    %   
+    %   Note the datapoints x_i, x_j are assumed to be *rows* of X.
+    %   
+    %   Specifically, W is constructed via the relation
+    %       W = (Q + XX') + (Q + XX')'
+    %       
+    %   where Q is the nxn matrix s.t. Q_ij = (||x_i||_2)^2
     
-    Note the datapoints x_i, x_j are assumed to be *rows* of X.
-    
-    Specifically, W is constructed via the relation
-        W = (Q + XX') + (Q + XX')'
-        
-    where Q is the nxn matrix s.t. Q_ij = (||x_i||_2)^2
-    
-    %}
-    
+   
     % construct in multiple steps. matlab can freeze for large n
     W = X.*X;
     W = repmat(sum(W,2), 1, size(X,1));
